475027d4016c1abd5719bf306c38b591dbd91577
BrunoRodriguesN <bruno_adeira@hotmail.com>
2024-08-08 14:59:15 -0300

    utilizando expects com propriedades

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test'
+import exp from 'constants'
 import { request } from 'http'
 
 test.describe.parallel("API Testing", () => {
  const baseURL= "https://reqres.in/api"   
-    test.only("Simple API Test - Assert Response status", async ({request}) => {
+    test("Simple API Test - Assert Response status", async ({request}) => {
         const response = await request.get(`${baseURL}/users/2`)
         // espera o resultado 200
         expect (response.status()).toBe(200)
@@ -16,6 +17,20 @@ test.describe.parallel("API Testing", () => {
         const response = await request.get(`${baseURL}/users/non-existing-endpoint`)
         // espera o resultado 404
         expect (response.status()).toBe(404)
+    })
+
+    test.only("GET Request - Get User Detail ", async ({request }) => {
+        const response = await request.get(`${baseURL}/users/1`)
+        const responseBody = JSON.parse(await response.text())
+
+        expect(response.status()).toBe(200)
+            //espera o data.id = 1
+            expect(responseBody.data.id).toBe(1)
+                expect(responseBody.data.first_name).toBe('George')
+                expect(responseBody.data.last_name).toBe('Bluth')
+                expect(responseBody.data.email).toBeTruthy()
+               // console.log(responseBody)
+
     })
 
 
