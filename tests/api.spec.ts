7acadedaa9b86d268a1bb24367f405afdd85239e
BrunoRodriguesN <bruno_adeira@hotmail.com>
2024-08-08 14:46:13 -0300

    utilizando JSONparse e mostrando a informacao

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -3,10 +3,13 @@ import { request } from 'http'
 
 test.describe.parallel("API Testing", () => {
  const baseURL= "https://reqres.in/api"   
-    test("Simple API Test - Assert Response status", async ({request}) => {
+    test.only("Simple API Test - Assert Response status", async ({request}) => {
         const response = await request.get(`${baseURL}/users/2`)
         // espera o resultado 200
         expect (response.status()).toBe(200)
+
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
     })
 
     test("Simple API Test - Assert Invalid Endpoint", async ({request}) => {
@@ -14,4 +17,6 @@ test.describe.parallel("API Testing", () => {
         // espera o resultado 404
         expect (response.status()).toBe(404)
     })
+
+
 })
